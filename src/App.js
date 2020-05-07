0f9591213e9ddaffacb9d64a4721ddc441af4800
Ash <ashwings1996@gmail.com>
2020-05-07 18:21:47 +0530

    tool tip commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactTooltip from "react-tooltip";
+import styled from 'styled-components';
 
-function App() {
+const Container = styled.div`
+background-color:lightblue;
+width:300px;
+margin:40px auto;
+padding:10px;
+text-align:center;
+`
+const Head = styled.div`
+background-color:black;
+color:white;
+width:100vh;
+margin:40px auto;
+padding:10px;
+text-align:center;
+`
+
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+       <Head> TOOL TIP </Head>
+      </h1>
+        <Container data-tip data-for='add'> ADD </Container>
+        <ReactTooltip id='add' type='success' place={props.pos} effect="solid" >
+          <span>Sure you want to Add the item</span>
+        </ReactTooltip>
+        <Container data-tip data-for='del'> DEL </Container>
+        <ReactTooltip id='del' type='warning' place={props.pos}>
+          <span>Sure you want to Del the item</span>
+        </ReactTooltip>
     </div>
   );
 }
